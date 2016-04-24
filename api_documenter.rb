a449f4e7ef9a4ba49b8b0460cdeba397f019cc6f
Conrad Kirby <conrad.kirby@gmail.com>
2016-04-24 16:58:23 -0400

    Added Groups. Refactored to allow the same function name to be used within nested scopes. Added custom linking. Updated example code.

--- api_documenter.rb
+++ api_documenter.rb
@@ -1,74 +1,106 @@
 module ApiDocumenter
   class Document
-    attr_accessor :title, :route, :routes
+    attr_accessor :context, :title, :description, :children
     def initialize
-      @routes = []
+      @context = self
+      @children = []
     end
     def content
-      retval = ''
-      @routes.each do |r|
-        retval << %Q{
-<div class='route' id='#{r.id}'>
-<p>
-<span class='verb'>#{r.verb}</span>
-<span class='uri'>#{r.uri}</span>
-</p>
-#{r.content}
-</div>
+      retval = %Q{
+<p class='title'>#{@title}</p>
+<p class='description'>#{parse_text(@description)}</p>
 }
-      end
+      @children.each { |c| retval << c.content }
       retval
     end
     def toc
-      retval = []
-      retval << "<div class='toc'>"
-      @routes.each do |r|
-        retval << "<a href='\##{r.id}'>#{r.verb} #{r.uri}</a>"
-      end
-      retval << "</div>"
-      retval.join "\n"
+      retval = %Q{
+<div class='toc'>
+}
+      @children.each { |c| retval << c.toc }
+      retval << %Q{
+</div>
+}
+    end
+  end
+
+  class Group
+    attr_accessor :name, :description, :id, :children
+    def initialize
+      @children = []
+    end
+    def content
+      retval = %Q{
+<div class='group' id='#{@id}'>
+<p class='name'>#{@name}</p>
+<p class='description'>#{parse_text(@description)}</p>
+}
+      @children.each { |c| retval << c.content }
+      retval << %Q{
+</div>
+}
+    end
+    def toc
+      retval = %Q{
+<div>
+<a href='\##{@id}'>#{@name}:</a>
+}
+      @children.each { |c| retval << c.toc }
+      retval << %Q{
+</div>
+}
     end
   end
 
   class Route
-    attr_accessor :verb, :uri, :context, :request
-    def id
-      (@verb + @uri).tr('/', '-')
-    end
+    attr_accessor :verb, :uri, :request, :id
     def content
       %Q{
-<div class='input'>
-<p>#{@request.description}</p>
-#{@request.content}
+<div class='route' id='#{@id}'>
+<p>
+<span class='verb'>#{@verb}</span>
+<span class='uri'>#{parse_uri}</span>
+</p>
+#{@request&.content}
 </div>
 }
+    end
+    def toc
+      %Q{
+<div><a href='\##{@id}'>#{@verb} #{parse_uri}</a></div>
+}
+    end
+    def parse_uri
+      retval = @uri&.gsub(/`([^`]*)`/, '&lt;\1&gt;')
     end
   end
 
   class Request
     attr_accessor :description, :spec
     def content
-      ret = %Q{
+      retval = %Q{
+<div class='request'>
+<p>#{parse_text(@description)}</p>
 <div class='spec'>
 <table>
 }
     @spec.table.each do |name, type, desc|
-      ret << %Q{
+      retval << %Q{
 <tr>
 <td class='name'>
 <span class='name'>#{name}</span>
 <span class='type'>#{type}</span>
 </td>
-<td class='description'>#{desc}</td>
+<td class='description'>#{parse_text(desc)}</td>
 </tr>
 }
     end
-    ret << %Q{
+    retval << %Q{
 </table>
-#{@spec.content}
+#{@spec&.content}
+</div>
 </div>
 }
-    ret
     end
   end
 
@@ -76,10 +108,11 @@ module ApiDocumenter
     attr_accessor :table, :example
     def content
       return unless @example      
-      ret = []
-      ret << "<div class='example'>"
-      ret << "<p>Example</p>"
-      ret << "<pre>{"
+      retval = %Q(
+<div class='example'>
+<p>Example</p>
+<pre>{
+)
       pre = []
       @example.each do |k, v|
         case v
@@ -91,17 +124,17 @@ module ApiDocumenter
           pre << %Q{  <span class='json-key'>"#{k}"</span>: <span class='json-token'>#{v}</span>}
         end
       end
-      ret << pre.join(",\n")
-      ret << "}</pre>"
-      ret << "</div>"
-      ret.join "\n"
+      retval << pre.join(",\n") << %Q(
+}</pre>
+</div>
+)
     end
   end
 
   module_function
   def document
     @d = Document.new
-    yield
+    wrap_context(@d) { yield }
     puts %Q{
 <!DOCTYPE html>
 <html lang='en'>
@@ -110,7 +143,7 @@ module ApiDocumenter
 <html>
 <head>
 <title>#{@d.title}</title>
-<link href='css/style.css' rel='stylesheet' type='text/css' />
+<link href='css/style.css' rel='stylesheet' type='text/css' charset='UTF-8' />
 </head>
 <body>
 #{@d.toc}
@@ -121,39 +154,43 @@ module ApiDocumenter
 </html>
 }
   end
-  def title t
-    @d.title = t
+  def group
+    g = Group.new
+    @d.context.children << g
+    wrap_context(g) { yield }
   end
-  def add_route
+  def route
     r = Route.new
-    @d.route = r
-    @d.routes << r
-    yield
-  end
-  def verb v
-    @d.route.verb = v
-  end
-  def uri u
-    @d.route.uri = u
+    @d.context.children << r
+    wrap_context(r) { yield }
   end
   def request
     r = Request.new
-    @d.route.request = r
-    @d.route.context = r
-    yield
-  end
-  def description d
-    @d.route.context.description = d
+    @d.context.request = r
+    wrap_context(r) { yield }
   end
   def spec
     s = Spec.new
-    @d.route.context.spec = s
+    @d.context.spec = s
+    wrap_context(s) { yield }
+  end
+  def method_missing method, *args
+    # forward setter calls to the object whose block we are in (@d.context)
+    # wrap_context() sets @d.context
+    new_method = "#{method}=".to_sym
+    super unless @d # endless loop protection
+    super unless @d.context.respond_to?(new_method) # endless loop protection
+    @d.context.send(new_method, *args)
+  end
+  def wrap_context new_context
+    # set the context before diving into the block
+    prev_context = @d.context
+    @d.context = new_context
     yield
+    # then set it back when we exit out
+    @d.context = prev_context
   end
-  def table t
-    @d.route.context.spec.table = t
-  end
-  def example e
-    @d.route.context.spec.example = e
+  def parse_text text
+    text.gsub(/\[([^\]:]*):([^\]]*)\]/, "<a href='\#\\1' class='link'>\\2</a>")
   end
 end
--- example.rb
+++ example.rb
@@ -3,39 +3,119 @@ include ApiDocumenter
 
 document do
   title 'Library'
-  add_route do
-    verb 'POST'
-    uri '/book'
-    request do
-      description 'Create a new book.'
-      spec do
-        table [
-          ['title', 'string', 'Title of the book.'],
-          ['author', 'string', 'Author of the book.'],
-          ['copies', 'integer', 'Number of copies of this book in the library.']
-        ]
-        example(
-          'title': 'A Tale of Two Cities',
-          'author': 'Charles Dickens',
-          'copies': 5
-        )
+  description 'API for managing a library.'
+  group do
+    name 'Books'; id 'books'
+    description 'API calls for the books object.'
+    route do 
+      verb 'POST'; uri '/books'; id 'books-create'
+      request do
+        description 'Create a new book.'
+        spec do
+          table [
+            ['title', 'string', 'Title of the book.'],
+            ['author', 'string', 'Author of the book.'],
+            ['copies', 'integer', 'Number of copies of the book in the library.']
+          ]
+          example(
+            title: 'A Tale of Two Cities',
+            author: 'Charles Dickens',
+            copies: 5
+          )
+        end
+      end
+    end
+    route do
+      verb 'PATCH'; uri '/books/`id`'; id 'books-update'
+      request do
+        description 'Update the properties of a book.'
+        spec do
+          table [
+            ['title', 'string', 'Title of the book.'],
+            ['author', 'string', 'Author of the book.'],
+            ['copies', 'integer', 'Number of copies of the book in the library.']
+          ]
+          example(
+            copies: 4
+          )
+        end
       end
     end
   end
-  add_route do
-    verb 'PATCH'
-    uri '/book/id'
-    request do
-      description 'Update a book\'s properties.'
-      spec do
-        table [
-          ['title', 'string', 'Title of the book.'],
-          ['author', 'string', 'Author of the book.'],
-          ['copies', 'integer', 'Number of copies of this book in the library.']
-        ]
-        example(
-          'copies': 4
-        )
+  group do
+    name 'Staff'; id 'staff'
+    description 'API calls for managing staff members.'
+    group do
+      name 'Employees'; id 'employees'
+      description 'Staff members earning wages.'
+      route do
+        verb 'POST'; uri '/employees'; id 'employees-create'
+        request do
+          description 'Create a new employee.'
+          spec do
+            table [
+              ['name', 'string', 'Name of the employee.'],
+              ['title', 'string', 'Job title of the employee.'],
+              ['favorite', 'integer', 'ID of their favorite [books:book].']
+            ]
+            example(
+              name: 'Bob Loblaw',
+              title: 'Internet Marketing Manager',
+              favorite: 1
+            )
+          end
+        end
+      end
+      route do
+        verb 'PATCH'; uri '/employees/`id`'; id 'employees-update'
+        request do
+          description 'Update the properties of an employee.'
+          spec do
+            table [
+              ['name', 'string', 'Name of the employee.'],
+              ['title', 'string', 'Job title of the employee.'],
+              ['favorite', 'integer', 'ID of their favorite [books:book].']
+            ]
+            example(
+              title: 'Janitor'
+            )
+          end
+        end
+      end
+    end
+    group do
+      name 'Volunteers'; id 'volunteers'
+      description 'Staff members not earning wages.'
+      route do
+        verb 'POST'; uri '/volunteers'; id 'volunteers-create'
+        request do
+          description 'Create a new volunteer.'
+          spec do
+            table [
+              ['name', 'string', 'Name of the volunteer.'],
+              ['title', 'string', 'Job title of the volunteer.']
+            ]
+            example(
+              name: 'Vin Diesel',
+              title: 'Bookmobile Driver'
+            )
+          end
+        end
+      end
+      route do
+        verb 'PATCH'; uri '/volunteers/`id`'; id 'volunteers-update'
+        request do
+          description 'Update the properties of a volunteer.'
+          spec do
+            table [
+              ['name', 'string', 'Name of the volunteer.'],
+              ['title', 'string', 'Job title of the volunteer.']
+            ]
+            example(
+              title: 'Library Assistant'
+            )
+          end
+        end
       end
     end
   end
